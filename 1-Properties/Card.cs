928078ac10935d7560708da95858479a90473da9
Giulia Bonifazi - giulia.bonifazi@studio.unibo.it <giulia.bonifazi@studio.unibo.it>
2022-12-05 11:55:04 +0100

    primo es, corretto con soluzioni

--- 1-Properties/Card.cs
+++ 1-Properties/Card.cs
@@ -7,9 +7,9 @@ namespace Properties
     /// </summary>
     public class Card
     {
-        private readonly string seed;
-        private readonly string name;
-        private readonly int ordinal;
+        private readonly string _seed;
+        private readonly string _name;
+        private readonly int _ordinal;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Card"/> class.
@@ -19,9 +19,9 @@ namespace Properties
         /// <param name="ordinal">the ordinal number of the card.</param>
         public Card(string name, string seed, int ordinal)
         {
-            this.name = name;
-            this.ordinal = ordinal;
-            this.seed = seed;
+            _name = name;
+            _ordinal = ordinal;
+            _seed = seed;
         }
 
         /// <summary>
@@ -33,32 +33,37 @@ namespace Properties
         }
 
         // TODO improve
-        public string GetSeed()
+        public string Seed
         {
-            return this.seed;
+            get => _seed; 
         }
-
         // TODO improve
-        public string GetName()
-        {
-            return this.name;
+        public string Name {
+            get => _name;
         }
-
         // TODO improve
-        public int GetOrdinal()
-        {
-            return this.ordinal;
+        public int Ordinal {
+            get => _ordinal;
         }
 
         /// <inheritdoc cref="object.ToString"/>
         public override string ToString()
         {
             // TODO understand string interpolation
-            return $"{this.GetType().Name}(Name={this.GetName()}, Seed={this.GetSeed()}, Ordinal={this.GetOrdinal()})";
+            return $"{this.GetType().Name}(Name={this.Name}, Seed={this.Seed}, Ordinal={this.Ordinal})";
         }
 
-        // TODO generate Equals(object obj)
+        public override bool Equals(object obj)
+        {
+            return obj is Card card &&
+                   _seed == card.Name &&
+                   _name == card.Name&&
+                   _ordinal == card.Ordinal;
+        }
 
-        // TODO generate GetHashCode()
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_seed, _name, _ordinal);
+        }
     }
 }
--- 1-Properties/DeckFactory.cs
+++ 1-Properties/DeckFactory.cs
@@ -9,57 +9,41 @@ namespace Properties
     /// </summary>
     public class DeckFactory
     {
-        private string[] seeds;
+        private string[] _seeds;
 
-        private string[] names;
+        private string[] _names;
 
-        // TODO improve
-        public IList<string> GetSeeds()
-        {
-            return this.seeds.ToList();
+        public IList<string> Seeds {
+            get => _seeds.ToList();
+            set => _seeds = value.ToArray();
+        }
+
+        public IList<string> Names {
+            get => _names.ToList();
+            set => _names = value.ToArray();
         }
 
         // TODO improve
-        public void SetSeeds(IList<string> seeds)
-        {
-            this.seeds = seeds.ToArray();
-        }
-
-        // TODO improve
-        public IList<string> GetNames()
-        {
-            return this.names.ToList();
-        }
-
-        // TODO improve
-        public void SetNames(IList<string> names)
-        {
-            this.names = names.ToArray();
-        }
-
-        // TODO improve
-        public int GetDeckSize()
-        {
-            return this.names.Length * this.seeds.Length;
-        }
+        public int GetDeckSize() => _names.Length * _seeds.Length;
 
         /// TODO improve
-        public ISet<Card> GetDeck()
-        {
-            if (this.names == null || this.seeds == null)
-            {
-                throw new InvalidOperationException();
-            }
+        public ISet<Card> Deck {
+            get {
+                if (_names == null || _seeds == null)
+                {
+                    throw new InvalidOperationException();
+                }
 
-            return new HashSet<Card>(Enumerable
-                .Range(0, this.names.Length)
-                .SelectMany(i => Enumerable
-                    .Repeat(i, this.seeds.Length)
-                    .Zip(
-                        Enumerable.Range(0, this.seeds.Length),
-                        (n, s) => Tuple.Create(this.names[n], this.seeds[s], n)))
-                .Select(tuple => new Card(tuple))
-                .ToList());
+                return new HashSet<Card>(Enumerable
+                    .Range(0, _names.Length)
+                    .SelectMany(i => Enumerable
+                        .Repeat(i, _seeds.Length)
+                        .Zip(
+                            Enumerable.Range(0, _seeds.Length),
+                            (n, s) => Tuple.Create(_names[n], _seeds[s], n)))
+                    .Select(tuple => new Card(tuple))
+                    .ToList());
+            }
         }
     }
 }
